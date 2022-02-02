17225e94af8bff6f0cc17beda8c630f359153a9f
CharlesBoydelaTour <charles.boydelatour@gmail.com>
2022-02-02 11:12:30 +0100

    adding negative samples first attemp

--- skipGram.py
+++ skipGram.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import argparse
 import pandas as pd
+import random
 
 # useful stuff
 import numpy as np
@@ -13,6 +14,7 @@ __emails__  = ['charles.boy-de-la-tour@student-cs.fr','gabriel.drai@student-cs.f
 
 def text2sentences(path):
 	# feel free to make a better tokenization/pre-processing
+	####### How to go deeper in tokenization ?
 	sentences = []
 	with open(path) as f:
 		for l in f:
@@ -59,31 +61,31 @@ def word_to_index(vocab):
 			count += 1
 	return w2id, id2w
 
+
 class SkipGram:
 	def __init__(self, sentences, nEmbed=100, negativeRate=5, winSize = 5, minCount = 5):
-		## TODO-start
+		
 		self.trainset = sentences
 		self.vocab = get_vocab(self.trainset)
 		self.w2id, self.id2w = word_to_index(self.vocab)
 		self.winSize = winSize
 		self.nEmbed = nEmbed
 		self.negativeRate = negativeRate
-
-		self.l1 = np.random.randn(len(self.vocab), self.nEmbed)*0.01
-		self.l2 = np.random.randn(self.nEmbed, len(self.vocab))*0.01
-		self.all_losses = []
+		self.learning_rate = 0.01
   
+		self.l1 = np.random.randn(len(self.vocab), self.nEmbed)*0.01 # word embedding layer, each row represents a word
+		self.l2 = np.random.randn(self.nEmbed, len(self.vocab))*0.01 # dense layer (for each word, what is the probability given input word)
+  
+
+		self.all_losses = []
+
+
 	def sample(self, omit):
-		"""samples negative words, ommitting those in set omit"""
-		## TODO-start   
-		omit = np.array(list(omit)) #index to omit
-		rng = np.random.default_rng() #create numpy random number generator
-		indexes = np.arange(len(self.vocab)) #array of all the possible indexes in vocab
-		samplesID = rng.choice(indexes, size=self.negativeRate, replace=False)
-		mask = np.isin(samplesID, omit, assume_unique=True, invert=True)
-		samplesID = samplesID[mask]
-		return samplesID.tolist()
-		## TODO-end
+		omit = (self.id2w[omit[0]], self.id2w[omit[1]])
+		vocab_without_omit = [x for x in self.vocab if x not in omit]
+		neg = random.sample(vocab_without_omit, self.negativeRate)
+		neg = [self.w2id[x] for x in neg]
+		return neg
 
 	def train(self):
 		self.trainWords = 0
@@ -99,7 +101,7 @@ class SkipGram:
 				ctxtIds = [self.w2id[x] for x in sentence[start:end] if x != word]
 
 				for ctxtId in ctxtIds:
-					negativeIds = self.sample((wIdx, ctxtId))
+					negativeIds = 0#self.sample((wIdx, ctxtId))
 					self.accLoss += self.trainWord(wIdx, ctxtId, negativeIds)
 					self.trainWords += 1
 		loss = self.accLoss/self.trainWords
@@ -107,65 +109,62 @@ class SkipGram:
 		print("Cross Entropy Loss: ", loss)
 
 	def trainWord(self, wordId, contextId, negativeIds):
-		context = np.zeros((len(self.vocab),1))
-		context[contextId] = 1
-		neg = np.zeros((len(self.vocab),1))
-		neg[negativeIds] = 1
-		h = self.l1[wordId,:]
-		h = h.reshape((len(h),1))
-		y_pred = expit(np.dot(self.l2.T, h))
-		#loss without netgativesIds
-		loss = -np.log(y_pred[contextId])
-		#loss with netgativesIds
-		for i in range(len(negativeIds)):
-			loss -= np.log(-y_pred[negativeIds[i]])
-		#derivates
-		dl2 = np.dot(y_pred - context, h.T)
-		dl1 = np.dot(self.l2, y_pred - context)
-		dl1 = dl1.T.reshape(dl1.shape[0])
-		#update
-		self.l2 = self.l2 - 0.02*dl2.T
-		self.l1[wordId,:] = self.l1[wordId,:] - 0.02*dl1
-		return np.sum(loss)
+		#Forward pass
+		l1 = self.l1[wordId,:] # selecting the row i of the word embedding layer
+		z = np.dot(l1, self.l2) # dot product of the row i and the dense layer
+		pred = expit(z) # applying softmax
+		#Cost function
+		cost = -np.log(pred[contextId]) - np.sum(np.log(-pred[negativeIds]))
+		#Stochastic gradient descent
+		pred[contextId] -= 1 
+		pred[negativeIds] -= 1 
+		l1_grad = np.dot(pred, self.l2.T)
+		l2_grad = np.dot(l1.reshape(self.nEmbed, 1), pred.reshape(1, len(self.vocab)))
+		self.l1[wordId,:] -= self.learning_rate * l1_grad
+		self.l2 -= self.learning_rate * l2_grad
+		return np.sum(cost)
 
+  
+  
 	def save(self, path):
-			np.save(path, np.array([self.l1,
-	             					self.l2,
-	                  				self.all_losses,
-	                      			self.id2w,
-	                         		self.w2id],
-	                         		dtype=object), 
-	          						allow_pickle=True)
+		np.save(path, np.array([self.l1,
+             					self.l2,
+                  				self.all_losses,
+                      			self.id2w,
+                         		self.w2id],
+                         		dtype=object),
+          						allow_pickle=True)
 
 	def similarity(self,word1,word2):
-			"""
-				computes similiarity between the two words. unknown words are mapped to one common vector
-			:param word1:
-			:param word2:
-			:return: a float \in [0,1] indicating the similarity (the higher the more similar)
-			"""
-			if word1 in self.w2id and word2 in self.w2id:
-				word1 = self.w2id[word1]
-				word2 = self.w2id[word2]
-				h1 = self.l1[word1,:]
-				h1 = h1.reshape((len(h1),1))
-				h1 = np.dot(self.l2.T, h1)
-				h1 = np.exp(h1) / np.sum(np.exp(h1), axis=0, keepdims=True)
-				h2 = self.l1[word2,:]
-				h2 = h2.reshape((len(h2),1))
-				h2 = np.dot(self.l2.T, h2)
-				h2 = np.exp(h2) / np.sum(np.exp(h2), axis=0, keepdims=True)
-				return np.dot(h1.T, h2)[0][0]
-			else:
-				return 0
+		"""
+			computes similiarity between the two words. unknown words are mapped to one common vector
+		:param word1:
+		:param word2:
+		:return: a float \in [0,1] indicating the similarity (the higher the more similar)
+		"""
+		if word1 in self.w2id and word2 in self.w2id:
+			word1 = self.w2id[word1]
+			word2 = self.w2id[word2]
+			h1 = self.l1[word1,:]
+			h1 = h1.reshape((len(h1),1))
+			h1 = np.dot(self.l2.T, h1)
+			h1 = np.exp(h1) / np.sum(np.exp(h1), axis=0, keepdims=True)
+			h2 = self.l1[word2,:]
+			h2 = h2.reshape((len(h2),1))
+			h2 = np.dot(self.l2.T, h2)
+			h2 = np.exp(h2) / np.sum(np.exp(h2), axis=0, keepdims=True)
+			return np.dot(h1.T, h2)[0][0]
+		else:
+			return 0
 
 	def load(self, path):
-			tmp = np.load(path+".npy", allow_pickle=True)
-			self.l1 = tmp[0]
-			self.l2 = tmp[1]
-			self.all_losses = tmp[2]
-			self.id2w = tmp[3]
-			self.w2id = tmp[4]
+		tmp = np.load(path+".npy", allow_pickle=True)
+		self.l1 = tmp[0]
+		self.l2 = tmp[1]
+		self.all_losses = tmp[2]
+		self.id2w = tmp[3]
+		self.w2id = tmp[4]
+
 
 
 
@@ -192,3 +191,4 @@ if __name__ == '__main__':
 		for a,b,_ in pairs:
             # make sure this does not raise any exception, even if a or b are not in sg.vocab
 			print('{:f}'.format(sg.similarity(a,b)))
+
